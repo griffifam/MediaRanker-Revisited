5882f8685f23e7c0cceab37ab0ccaffb42d9c08e
Amber Lynn <griffiam@gmail.com>
2018-10-15 14:22:51 -0700

    Added an index test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,38 @@
 require 'test_helper'
 
 describe UsersController do
+  describe User do
+
+    describe "Index View" do
+      let(:user) { users(:one) }
+
+      it "should go to index view" do
+        get users_path
+
+        must_respond_with :success
+      end
+    end
+
+    #ARRANGE
+
+    #ACT
+
+
+    #ASSERT
+
+    describe "Show View" do
+      let(:user) { users(:one) }
+
+      it "should go to show index" do
+        #ARRANGE
+        
+
+        #ACT
+
+
+        #ASSERT
+      end
+    end
+  end
 
 end
